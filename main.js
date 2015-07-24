99cc75856134d5e1d32e3c2b2540eb41a6ae7d81
刺身魚 <atto_999@hotmail.com>
2015-07-25 04:33:56 +0900

    あさ来る前に寝る

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
 var Lock = require('lib/lock.js');
-var gpio = require('gpio');
+var gpio = require('pi-gpio');
 
 var lock = new Lock(gpio);
 lock.attach({
-	inA:5
-	,inB:6
-	,outA:13
-	,outB:19
-	,motor1:20
-	,motor2:21
+	inA:3
+	,inB:5
+	,outA:7
+	,outB:24
+	,motor1:17
+	,motor2:27
 });
 
 app.use(bodyParser.json());
